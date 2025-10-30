a36a7c6a34031f95a8a6924bc24d26990dc717a2
Doody06 <Abdelrahmanschool06@gmail.com>
2025-10-30 09:44:55 +0300

    Fixed the errors in Faculty and SupportEmployee just by adding a constructor

--- src/main/java/org/cs101project/Faculty.java
+++ src/main/java/org/cs101project/Faculty.java
@@ -13,4 +13,11 @@ import org.cs101project.Employee;
 public class Faculty extends Employee {
     private String rank;
     private String specialisation;
+
+    public Faculty(String rank, String specialisation, String department, String officeNumber, String firstName, String surname, String username, String password, String dateOfBirth) {
+        super(department, officeNumber, firstName, surname, username, password, dateOfBirth);
+        this.rank = rank;
+        this.specialisation = specialisation;
+    }
+    
 }
--- src/main/java/org/cs101project/SupportEmployee.java
+++ src/main/java/org/cs101project/SupportEmployee.java
@@ -12,4 +12,11 @@ import org.cs101project.Employee;
  */
 public class SupportEmployee extends Employee {
     private String jobDescription;
+
+    public SupportEmployee(String jobDescription, String department, String officeNumber, String firstName, String surname, String username, String password, String dateOfBirth) {
+        super(department, officeNumber, firstName, surname, username, password, dateOfBirth);
+        this.jobDescription = jobDescription;
+    }
+    
+    
 }
