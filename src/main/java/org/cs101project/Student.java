3fcc7bab75eb5958b5975dbe686c9144b25e4c09
SALMAN_PSU <SALMAN_PSU@SALMAN>
2025-11-09 16:08:27 +0300

    (Format)

--- src/main/java/org/cs101project/Student.java
+++ src/main/java/org/cs101project/Student.java
@@ -1,14 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.cs101project;
 
-/**
- *
- * @author Abdelrahman
- */
 public class Student extends Person {
+
     private String status;
     private String major;
     private Award[] Awards;
@@ -43,6 +36,5 @@ public class Student extends Person {
     public void setAwards(Award[] Awards) {
         this.Awards = Awards;
     }
-    
-    
+
 }
