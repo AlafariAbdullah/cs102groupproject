442dbe2bb33090bd0bb33be5a7fed0d7728acadd
SALMAN_PSU <SALMAN_PSU@SALMAN>
2025-11-09 16:08:58 +0300

    (Format)

--- src/main/java/org/cs101project/Person.java
+++ src/main/java/org/cs101project/Person.java
@@ -1,12 +1,14 @@
 package org.cs101project;
 
-public class Person{
+public class Person {
+
     private String firstName;
     private String surName;
     private String userName;
     private String password;
     private String birthDate;
-    public Person(String n, String s, String u, String p, String bd){
+
+    public Person(String n, String s, String u, String p, String bd) {
         this.password = Cypher.encryptSubstitution(p);
     }
 }
