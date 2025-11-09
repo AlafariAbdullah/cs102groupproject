26eda3976d9578e4b10ed167be9855a5f0f65a26
SALMAN_PSU <SALMAN_PSU@SALMAN>
2025-11-09 16:08:17 +0300

    (Format)

--- src/main/java/org/cs101project/SupportEmployee.java
+++ src/main/java/org/cs101project/SupportEmployee.java
@@ -1,22 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.cs101project;
 
 import org.cs101project.Employee;
 
-/**
- *
- * @author Abdelrahman
- */
 public class SupportEmployee extends Employee {
+
     private String jobDescription;
 
     public SupportEmployee(String jobDescription, String department, String officeNumber, String firstName, String surname, String username, String password, String dateOfBirth) {
         super(department, officeNumber, firstName, surname, username, password, dateOfBirth);
         this.jobDescription = jobDescription;
     }
-    
-    
+
 }
