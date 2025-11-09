6f3914b6f6124d3142c517690c8af3744f401b8f
SALMAN_PSU <SALMAN_PSU@SALMAN>
2025-11-09 16:08:36 +0300

    (Format)

--- src/main/java/org/cs101project/Projectcs102.java
+++ src/main/java/org/cs101project/Projectcs102.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 package org.cs101project;
 
 import java.io.File;
 import java.io.PrintWriter;
-/**
- *
- * @author Abdullah
- */
+
 import java.util.Scanner;
 
 public class Projectcs102 {
@@ -30,79 +24,75 @@ public class Projectcs102 {
             String loginUsername = userInput.nextLine();
             System.out.println("Enter password: ");
             String loginPassword = userInput.nextLine();
-            
+
             boolean success = signIn(loginUsername, loginPassword);
-            
+
             if (success) {
                 System.out.println("Access granted!");
             } else {
                 System.out.println("Access denied.");
             }
         } else if (choice.equalsIgnoreCase("Sign up") || choice.equalsIgnoreCase("2")) {
-        System.out.println("Type (1) if you are a student, (2) if you are a faculty"
-                + "member, and (3) if you are a support employee");
-        System.out.print("Enter your choice: ");
-        String type = userInput.nextLine();
+            System.out.println("Type (1) if you are a student, (2) if you are a faculty"
+                    + "member, and (3) if you are a support employee");
+            System.out.print("Enter your choice: ");
+            String type = userInput.nextLine();
 
-        System.out.print("First name: ");
-        String fname = userInput.nextLine();
-        System.out.print("Last name: ");
-        String lname = userInput.nextLine();
-        System.out.print("Username: ");
-        String username = userInput.nextLine();
-        System.out.print("Date of birth: ");
-        String Date = userInput.nextLine();
-        System.out.print("Password: ");
-        String password = userInput.nextLine();
+            System.out.print("First name: ");
+            String fname = userInput.nextLine();
+            System.out.print("Last name: ");
+            String lname = userInput.nextLine();
+            System.out.print("Username: ");
+            String username = userInput.nextLine();
+            System.out.print("Date of birth: ");
+            String Date = userInput.nextLine();
+            System.out.print("Password: ");
+            String password = userInput.nextLine();
 
-        if (type.equals("1")) {
-            System.out.print("Status (Freshman, Sophomore, etc): ");
-            String status = userInput.nextLine();
-            System.out.print("Major: ");
-            String major = userInput.nextLine();
-            System.out.println("Student account created for " + fname + " " + lname);
-            saveUserToFile(type, fname, lname, username, Date, password,
-               status, major, null, null, null, null, null); 
-            //Null fields mean this parameter is irrelevant for this person type (student for example)
-        
-        } else if (type.equals("2")) {
-            System.out.print("Department: ");
-            String dept = userInput.nextLine();
-            System.out.print("Office number: ");
-            String office = userInput.nextLine();
-            System.out.print("Rank: ");
-            String rank = userInput.nextLine();
-            System.out.print("Specialization: ");
-            String spec = userInput.nextLine();
-            System.out.println("Faculty account created for " + fname + " " + lname);
-            saveUserToFile(type, fname, lname, username, Date, password,
-               null, null, dept, office, rank, spec, null);
-        
-        } else if (type.equals("3")) {
-            System.out.print("Department: ");
-            String dept = userInput.nextLine();
-            System.out.print("Office number: ");
-            String office = userInput.nextLine();
-            System.out.print("Job description: ");
-            String job = userInput.nextLine();
-            System.out.println("Support & Services account created for " + fname + " " + lname);
-            saveUserToFile(type, fname, lname, username, Date, password,
-               null, null, dept, office, null, null, job);
-        
-        } else {
-            System.out.println("Invalid choice");
-        }
-            
-        System.out.println("(Sign up is complete)");
-    }
-        
-         else if (choice.equalsIgnoreCase("Exit") || choice.equalsIgnoreCase("3")) {
+            if (type.equals("1")) {
+                System.out.print("Status (Freshman, Sophomore, etc): ");
+                String status = userInput.nextLine();
+                System.out.print("Major: ");
+                String major = userInput.nextLine();
+                System.out.println("Student account created for " + fname + " " + lname);
+                saveUserToFile(type, fname, lname, username, Date, password,
+                        status, major, null, null, null, null, null);
+                //Null fields mean this parameter is irrelevant for this person type (student for example)
+
+            } else if (type.equals("2")) {
+                System.out.print("Department: ");
+                String dept = userInput.nextLine();
+                System.out.print("Office number: ");
+                String office = userInput.nextLine();
+                System.out.print("Rank: ");
+                String rank = userInput.nextLine();
+                System.out.print("Specialization: ");
+                String spec = userInput.nextLine();
+                System.out.println("Faculty account created for " + fname + " " + lname);
+                saveUserToFile(type, fname, lname, username, Date, password,
+                        null, null, dept, office, rank, spec, null);
+
+            } else if (type.equals("3")) {
+                System.out.print("Department: ");
+                String dept = userInput.nextLine();
+                System.out.print("Office number: ");
+                String office = userInput.nextLine();
+                System.out.print("Job description: ");
+                String job = userInput.nextLine();
+                System.out.println("Support & Services account created for " + fname + " " + lname);
+                saveUserToFile(type, fname, lname, username, Date, password,
+                        null, null, dept, office, null, null, job);
+
+            } else {
+                System.out.println("Invalid choice");
+            }
+
+            System.out.println("(Sign up is complete)");
+        } else if (choice.equalsIgnoreCase("Exit") || choice.equalsIgnoreCase("3")) {
             System.exit(1);
         }
     }
 
-       
-    
     public static boolean includes(String[] arr, String str) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i].equalsIgnoreCase(str)) {
@@ -111,63 +101,64 @@ public class Projectcs102 {
         }
         return false;
     }
-    
+
     public static void saveUserToFile(
-        String type,
-        String fname,
-        String lname,
-        String username,
-        String dob,
-        String password,
-        String status,
-        String major,
-        String department,
-        String office,
-        String rank,
-        String specialization,
-        String job
-) {
-    try (PrintWriter pw = new PrintWriter(new File("users.txt"))) {
+            String type,
+            String fname,
+            String lname,
+            String username,
+            String dob,
+            String password,
+            String status,
+            String major,
+            String department,
+            String office,
+            String rank,
+            String specialization,
+            String job
+    ) {
+        try (PrintWriter pw = new PrintWriter(new File("users.txt"))) {
 
-        // Write all data in one line separated by commas, end with semicolon
-        pw.print(type + "," +
-                fname + "," +
-                lname + "," +
-                username + "," +
-                dob + "," +
-                password + "," +
-                status + "," +
-                major + "," +
-                department + "," +
-                office + "," +
-                rank + "," +
-                specialization + "," +
-                job + ";");
+            // Write all data in one line separated by commas, end with semicolon
+            pw.print(type + ","
+                    + fname + ","
+                    + lname + ","
+                    + username + ","
+                    + dob + ","
+                    + password + ","
+                    + status + ","
+                    + major + ","
+                    + department + ","
+                    + office + ","
+                    + rank + ","
+                    + specialization + ","
+                    + job + ";");
 
-        pw.println(); // new line for next user
+            pw.println(); // new line for next user
 
-    } catch (Exception e) {
-        System.out.println("Error writing to file: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error writing to file: " + e.getMessage());
+        }
     }
-}
+
     public static boolean signIn(String username, String password) {
         try (Scanner sc = new Scanner(new File("users.txt"))) {
             while (sc.hasNextLine()) {
                 String line = sc.nextLine().trim();
-                if(line.isEmpty()) {
+                if (line.isEmpty()) {
                     continue;
                 }
-                
-                if(line.endsWith(";")) {
+
+                if (line.endsWith(";")) {
                     line = line.substring(0, line.length() - 1);
                 }
-                
+
                 String[] fields = line.split(",");
-                
+
                 if (fields.length >= 6) {
                     String storedUsername = fields[3]; //field 3 is username no matter what
                     String storedPassword = fields[5]; //field 5 is password no matter what
-                    
+
                     if (storedUsername.equalsIgnoreCase(username) && storedPassword.equals(password)) {
                         System.out.println("Sign in successful. Welcome" + fields[1] + " " + fields[2] + "!");
                         return true; //WIP here should sign in
@@ -176,9 +167,8 @@ public class Projectcs102 {
             }
             System.out.println("Invalid username or password");
             return false; //WIP here should not sign in and continue loop in main
-        }
-        catch (Exception e) {
-            System.out.println("Error reading users file: "+ e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error reading users file: " + e.getMessage());
             return false; // WIP here should not sign in and continue loop in main
         }
     }
