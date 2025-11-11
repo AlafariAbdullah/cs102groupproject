4cedad69b832adc64ec714ea7203b8d37e616b2c
Abdullah A Alafari <58389155+AlafariAbdullah@users.noreply.github.com>
2025-11-11 12:17:12 +0300

    added Username already exists functionality:

--- src/main/java/org/cs101project/Projectcs102.java
+++ src/main/java/org/cs101project/Projectcs102.java
@@ -7,11 +7,14 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Projectcs102 {
-
+    private static HashMap<String,String> userPass;
     public static void main(String[] args) {
         // System.out.println("Hi");    
         Scanner userInput = new Scanner(System.in);
-
+       
+        userPass = loadHistory();
+       
+       
         // Main menu
         String[] possibleInput = {"Sing in", "Sign up", "Exit", "1", "2", "3"};
         String choice = null;
@@ -70,15 +73,23 @@ public class Projectcs102 {
                 System.exit(1);
             }
             while (!type.equals("1") && !type.equals("2") && !type.equals("3"));
-        
+       
 
             System.out.println("");
             System.out.print("First name: ");
             String fname = userInput.nextLine();
             System.out.print("Last name: ");
             String lname = userInput.nextLine();
+           
+           String username = "";
+           do{
+            if (usernameExists(username))
+                System.out.println("Username already exists! Try again!");
             System.out.print("Username: ");
-            String username = userInput.nextLine();
+            username = userInput.nextLine();
+           }while (usernameExists(username));
+
+           
             System.out.print("Date of birth: ");
             String Date = userInput.nextLine();
             System.out.print("Password: ");
@@ -118,7 +129,7 @@ public class Projectcs102 {
                     System.out.println("Faculty account created for " + fname + " " + lname);
                     saveUserToFile(new Faculty(fname, lname, username, password, Date, type, dept, office, rank, spec));
                     break;
-    
+   
                 }
                 case "3": {
                     type = "SupportEmployee" ;
@@ -133,14 +144,14 @@ public class Projectcs102 {
                     break;
                 }
             }
-            
+           
         }
             else if (choice.equalsIgnoreCase("Exit") || choice.equalsIgnoreCase("3")) {
                 System.exit(1);
 
             System.out.println("(Sign up is complete)");
             }
-        } 
+        }
 
 
     public static boolean includes(String[] arr, String str) {
@@ -189,9 +200,35 @@ public class Projectcs102 {
     }
 
 
+   public static boolean usernameExists(String username){
+        return userPass.containsKey(username);
+   
+   }
 
+    public static HashMap<String, String> loadHistory(){
+        HashMap<String, String> usernamePassword = new HashMap<>();
+        StringBuilder block = new StringBuilder();
+        try (Scanner sc = new Scanner(new File("users.txt"))) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (line.isEmpty()) continue;
+                block.append(line).append("\n");
+                if (line.endsWith(";")){
+                    String userData = block.toString();
+                    userData = userData.substring(0,userData.length()-1).trim();
+                    String[] fields = userData.split(",");
+                    usernamePassword.put(fields[2],fields[3]);
+                    block.setLength(0);
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Error reading users file: " + e.getMessage());
+               
+    }
+        return usernamePassword;
 
-
+    }
     public static boolean signIn(String username, String password) {
         password = Cipher.encryptSubstitution(password);
         HashMap<String, String> usernamePassword = new HashMap<>();
@@ -203,19 +240,19 @@ public class Projectcs102 {
                 block.append(line).append("\n");
                 if (line.endsWith(";")){
                     String userData = block.toString();
-                    userData.substring(0,userData.length()-1).trim();
+                    userData = userData.substring(0,userData.length()-1).trim();
                     String[] fields = userData.split(",");
                     usernamePassword.put(fields[2],fields[3]);
                     block.setLength(0);
                 }
             }
-        } 
+        }
         catch (Exception e) {
             System.out.println("Error reading users file: " + e.getMessage());
             return false; // WIP here should not sign in and continue loop in main
         }
         return usernamePassword.containsKey(username) && usernamePassword.get(username).equals(password);
 
-    
+   
     }
 }
